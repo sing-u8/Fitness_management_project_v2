90cc872e7455a92801bcbe0726a2e186bb969534
ParkSingyu <singyu13@gmail.com>
2023-02-15 18:20:36 +0900

    -- FEA : cypress example component

---
+++ src/app/stepper/stepper.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
+
+@Component({
+    selector: 'app-stepper',
+    templateUrl: 'stepper.component.html',
+})
+export class StepperComponent {
+    constructor() {}
+
+    ngOnInit(): void {}
+
+    @Input() count = 0
+    @Output() change = new EventEmitter()
+
+    increment(): void {
+        this.count++
+        this.change.emit(this.count)
+    }
+
+    decrement(): void {
+        this.count--
+        this.change.emit(this.count)
+    }
+}

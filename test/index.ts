d8bc8fd3dff4f8eda75c97afe3fb341fba80c707
ParkSingyu <singyu13@gmail.com>
2023-03-02 17:27:59 +0900

    -- FEA : 단위/컴포넌트 테스트에 쓰이는 유틸리티 함수를 위한 test 폴더 생성 및 index.ts 파일 생성, 경로 설정

---
+++ test/index.ts
@@ -0,0 +1,25 @@
+import { DebugElement } from '@angular/core'
+import { tick, ComponentFixture } from '@angular/core/testing'
+
+// utilities
+/** Wait a tick, then detect changes */
+export function advance(f: ComponentFixture<any>): void {
+    tick()
+    f.detectChanges()
+}
+
+// See https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
+/** RouterLink 이벤트 핸들러를 위해 버튼 이벤트 객체의 일부를 다시 선언합니다. */
+export const ButtonClickEvents = {
+    left: { button: 0 },
+    right: { button: 2 },
+}
+
+/** 엘리먼트 클릭을 처리합니다. 이벤트 객체의 기본값은 마우스 왼쪽 버튼 클릭 이벤트입니다. */
+export function click(el: DebugElement | HTMLElement, eventObj: any = ButtonClickEvents.left): void {
+    if (el instanceof HTMLElement) {
+        el.click()
+    } else {
+        el.triggerEventHandler('click', eventObj)
+    }
+}

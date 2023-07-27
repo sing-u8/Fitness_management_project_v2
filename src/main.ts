456d582d12ad0dfe65480818581398235dd96dcb
ParkSingyu <singyu13@gmail.com>
2023-07-27 17:55:55 +0900

    -- MODIFY :     - main.ts import 코드 정리

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core'
 import { bootstrapApplication, BrowserModule } from '@angular/platform-browser'
 import { provideRouter } from '@angular/router'
-import { AppComponent } from './app/app.component'
+import { COMPOSITION_BUFFER_MODE } from '@angular/forms'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutes } from '@routes/app.routes'
 
-import { environment } from '@environments/environment'
-import { HttpClientModule } from '@angular/common/http'
+// firebase
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app'
 import { getFirestore, provideFirestore } from '@angular/fire/firestore'
 import { getAuth, provideAuth } from '@angular/fire/auth'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AuthModule as AngularFireAuthModule } from '@angular/fire/auth/auth.module'
 import { StorageModule as AngularFirestoreModule } from '@angular/fire/storage/storage.module'
+
+// ngrx
 import { StoreModule } from '@ngrx/store'
 import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store'
 import { appFeatureKey, appReducer } from '@store/app/reducers/reducers'
@@ -19,8 +21,11 @@ import { metaReducers } from '@store/app/reducers/meta-reducers'
 import { EffectsModule } from '@ngrx/effects'
 import { AppEffect } from '@store/app/effects/effects'
 import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+
+// etc
+import { AppComponent } from './app/app.component'
+import { environment } from '@environments/environment'
 import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module } from 'ng-recaptcha'
-import { COMPOSITION_BUFFER_MODE } from '@angular/forms'
 import { CoreModule } from './app/core/core.module'
 
 if (environment.production) {

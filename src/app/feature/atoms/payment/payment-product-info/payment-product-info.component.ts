a963498777cc9f5f8c95da4f8e78a7906393de7c
ParkSingyu <singyu13@gmail.com>
2023-08-09 12:46:32 +0900

    -- MODIFY :     - [월 이용권] 결제일 문구 변경     - 불필요한 코드 삭제

--- src/app/feature/atoms/payment/payment-product-info/payment-product-info.component.ts
+++ src/app/feature/atoms/payment/payment-product-info/payment-product-info.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { SharedModule } from '@shared/shared.module'
 import { PaymentItem, PaymentItemInfo } from '@schemas/payment/payment-item'
+import { changesOn, detectChangesOn } from "@shared/helper/component-helper";
+import dayjs from 'dayjs'
 
 @Component({
     selector: 'rwa-payment-product-info',
@@ -10,10 +12,19 @@ import { PaymentItem, PaymentItemInfo } from '@schemas/payment/payment-item'
     templateUrl: './payment-product-info.component.html',
     styleUrls: ['./payment-product-info.component.scss'],
 })
-export class PaymentProductInfoComponent {
+export class PaymentProductInfoComponent implements OnChanges {
     @Input() paymentItem: PaymentItem
     @Input() paymentItemInfo: PaymentItemInfo
 
     public productDetailOpen = true
+
+    public subscriptionPaymentDate = dayjs().format('YYYY-MM-DD') // 에러 방지용 초기값
     constructor() {}
+    ngOnChanges(changes: SimpleChanges) {
+        detectChangesOn(changes, 'paymentItemInfo', (v) => {
+            this.subscriptionPaymentDate = dayjs(this.paymentItemInfo.period.endDate)
+                .add(1, 'day')
+                .format('YY년 MM월 DD일')
+        })
+    }
 }

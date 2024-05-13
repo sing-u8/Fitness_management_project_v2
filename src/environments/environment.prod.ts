70a7fcdca40261db75beaefd2762c5c167114b42
ParkSingyu <singyu13@gmail.com>
2024-05-13 20:02:13 +0900

    -- finish.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,58 +2,23 @@ export const environment = {
     production: true,
     protocol: 'https://',
     subDomain: 'api.',
-    prodSubDomain: 'v3api.',
-    domain: 'redwhale.xyz',
-    port: ':443',
-    version: '',
-    wss: 'wss://15s5c1lahf.execute-api.ap-northeast-2.amazonaws.com/prod',
-    firebase: {
-        projectId: 'redwhale-f9890',
-        appId: '1:830659387924:web:6ad49776c58835ac10bd01',
-        storageBucket: 'redwhale-f9890.appspot.com',
-        apiKey: 'AIzaSyDM4worb5VgtYUTH9O6iBJBrKp2v42Orps',
-        authDomain: 'redwhale-f9890.firebaseapp.com',
-        messagingSenderId: '830659387924',
-        measurementId: 'G-968N2LKCEZ',
-    },
-    kakao: {
-        appKey: {
-            javascript: '80f7f2934ed2250481af1936d4de9da5',
-        },
-    },
-    RECAPTCHA_SITE_KEY: '6Lft2NMZAAAAABaEafU1cNe_aXgjCkZgYYDFEIet',
-}
-
-/*
-
-export const environment = {
-    production: true,
-    protocol: 'https://',
-    subDomain: 'api.',
     domain: 'redwhale.xyz',
     port: ':443',
     version: '/latest',
-    wss: 'wss://nwa565188k.execute-api.ap-northeast-2.amazonaws.com/dev',
+    wss: '',
     firebase: {
-        apiKey: 'AIzaSyBfX29oNTYgHYCOobsrCXpRXKhTxvdpenI',
-        authDomain: 'redwhale-f9a21.firebaseapp.com',
-        databaseURL: 'https://redwhale-f9a21.firebaseio.com',
-        projectId: 'redwhale-f9a21',
-        storageBucket: 'redwhale-f9a21.appspot.com',
-        messagingSenderId: '31023318401',
-        appId: '1:31023318401:web:4a81ab87ee92b45af3d480',
-        measurementId: 'G-8MFGNK2B6L',
+        projectId: 'redwhale',
+        appId: '',
+        storageBucket: '',
+        apiKey: '',
+        authDomain: '',
+        messagingSenderId: '',
+        measurementId: '',
     },
     kakao: {
         appKey: {
-            javascript: '238fca776f9dde773b05c213f22e642b',
+            javascript: 'xx',
         },
     },
-    RECAPTCHA_SITE_KEY: '6Lft2NMZAAAAABaEafU1cNe_aXgjCkZgYYDFEIet',
+    RECAPTCHA_SITE_KEY: 'xxx',
 }
-
-
-
-
-
-*/
